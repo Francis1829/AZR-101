f6111439ae12db6e4822321d6068bbc188c5d870
Francis Mar Subsilica <143585435+Francis1829@users.noreply.github.com>
2024-03-26 18:26:07 +0900

    onti nalang

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,6 +13,7 @@ function Hero() {
               <div className=' 2xl:text-[3rem] xl:text-[2rem] text-[2.5rem] font-[Catamaran]'>We Provide Top-Tier Virtual Services</div>
               <div className=' 2xl:text-[4.5rem] text-[#ad8d3d] xl:text-[3.5rem] text-[4rem] font-[Anton] uppercase'>Elevate Your Future with Remote Excellence</div>
               <div className=' 2xl:text-[2rem] xl:text-[1.5rem] text-[2rem] font-[Catamaran]'>Precisely Matching Our Client's Objective</div>
+              <div className='2xl:w-[45vmin] xl:w-[45vmin] 2xl:h-[60vmin] xl:h-[60vmin] bg-[#a9944e]/50 rounded-b-full absolute top-0 2xl:-left-10 -left-0 -z-10'></div>
           </div>
           <div className='w-[50%] h-full 2xl:flex xl/lg:flex hidden flex-col justify-end items-end relative z-10'>
             <div className='2xl:w-[65vmin] xl:w-[70vmin] 2xl:h-[85vmin] xl:h-[80vmin] bg-[#a9944e]/50 absolute bottom-0 2xl:-right-10 -right-0 -z-10 rounded-t-full'></div>
